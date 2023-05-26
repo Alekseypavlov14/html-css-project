c24743f2091b35bb2ce1a3e0c6088038d6b85368
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-05-26 11:12:39 +0300

    fixed menu

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
-  const tabIndex = window.location.href.split('#').reverse()[0] - 1
+  let tabIndex = 0
+
+  try {
+    tabIndex = Number(window.location.href.split('#').reverse()[0]) - 1
+  } catch(e) {} 
+
   $(".tabs li").removeClass("active");
   $('.tabs li').eq(tabIndex).addClass("active");
+
+  $(".myTable").hide();
   $(".myTable").eq(tabIndex).show();
+
+  window.onhashchange = (e) => {
+    let tabIndex = 0
+
+    try {
+      tabIndex = Number(window.location.href.split('#').reverse()[0]) - 1
+    } catch(e) {} 
+
+    $(".tabs li").removeClass("active");
+    $('.tabs li').eq(tabIndex).addClass("active");
+
+    $(".myTable").hide();
+    $(".myTable").eq(tabIndex).show();
+  }
 
   $(".tabs li").click(function() {
     var tabIndex = $(this).index();
@@ -20,6 +41,12 @@ $(document).ready(function() {
     $('.sidebar__link').not(this).removeClass('sidebar__link--active')
     $(this).toggleClass('sidebar__link--active')
   })
+  
+  $('.sidebar__link__submenu__link').click(function() {
+    $('.sidebar__link__submenu__link').removeClass('sidebar__link__submenu__link--active')
+    $(this).addClass('sidebar__link__submenu__link--active')
+  })
+
   try {
     $('input[name="dates"]').daterangepicker();
   } catch(e) {}
